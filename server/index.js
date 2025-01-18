37128f50f5116b6c3d9506666019e7406dda87e1
QuanMBL <162358434+QuanMBL@users.noreply.github.com>
2025-01-18 09:22:46 +0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+//Sử dụng middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+//Routes API
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from Server!' })
+})
+//Tạo web server
+app.listen(PORT, () =>
+    console.log('Server is running at port ${PORT}')
+);
